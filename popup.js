70256facdf6dc5ef1f607c98659a54dfd7422ab0
Avery <74130268+velara@users.noreply.github.com>
2023-01-02 17:18:03 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -28,7 +28,7 @@ Hooks.once('ready', async function () {
 								</h3>
 								<p>
 								This FoundryVTT module has been prepared by Avery for Eldritch Osiris Games, please report any bugs to: 
-<a href="https://github.com/Eldritch-Osiris-Games/foundry-pf2e-monday-monsters-vol1/issues">https://github.com/Eldritch-Osiris-Games/foundry-pf2e-monday-monsters-vol1/issues</a>
+								<a href="https://github.com/Eldritch-Osiris-Games/foundry-pf2e-monday-monsters-vol1/issues">https://github.com/Eldritch-Osiris-Games/foundry-pf2e-monday-monsters-vol1/issues</a>
 								</p>
                 `,
                 buttons: {
